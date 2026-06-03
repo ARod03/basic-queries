96ac451ceef6bb8ede66d4fb3edfa41488a85f29
ARod03 <ardgz2003@gmail.com>
2026-06-02 20:02:22 -0500

    Question 10
    Script to find all seafood items sold.

---
+++ 10_all_seafood_items.sql
@@ -0,0 +1,8 @@
+SELECT
+	*
+FROM
+	northwind. products
+WHERE
+	categoryID = 8;
+/* The category ID is what determines the type of each item sold. All of the products with
+the categoryID number 8 is all seafood.
