ebfd0277f9398e34c119ca2b07ba322105237a23
ARod03 <ardgz2003@gmail.com>
2026-06-02 20:17:52 -0500

    Question 11
    Script to find employees' first and last name.

---
+++ 11_employee_first_last_name.sql
@@ -0,0 +1,6 @@
+SELECT
+	FirstName, LastName
+FROM
+	northwind. employees
+/* The employees are Nancy Davolio, Andrew Fuller, Janet Leverling, Margaret Peacock,
+Steven Buchanan, Michael Suyama, Robert King, Laura Callahan, and Anne Dodsworth*/
