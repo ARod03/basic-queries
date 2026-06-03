55418ee94b76e43a691672ae953ce1d1fd5781c7
ARod03 <ardgz2003@gmail.com>
2026-06-02 19:30:56 -0500

    Question 6
    Script to find products with at least 100 units in descending order by price, but ascending order for product name if the price is the same between products.

---
+++ 06_products_at_least_100_units_desc_price_asc_product_name.sql
@@ -0,0 +1,11 @@
+SELECT
+	ProductID, ProductName, UnitPrice, UnitsInStock
+FROM
+	northwind. products
+WHERE
+	UnitsInStock >= 100
+ORDER BY
+	UnitPrice DESC, ProductName ASC;
+/* The products with at least 100 units in stock are: Sirop d'rable, Grandma's Boysenberry Spread,
+Pt chinois, Gustaf's Knckebrd, Inlagd Sill, Boston Crab Meat, Rd Kaviar, Sasquatch Ale,
+Rhnbru Klosterbier, Geitost */
