02efafc580b2469a11a2bfde406c974b528d5273
ARod03 <ardgz2003@gmail.com>
2026-06-02 21:06:20 -0500

    Question 13
    Script to find the distinct job titles of Northwind.

---
+++ 13_distinct_job_titles.sql
@@ -0,0 +1,6 @@
+SELECT
+	DISTINCT title
+FROM
+	northwind. employees
+/* The distinct job titles for the employees of the company are: Sales Representative, 
+Vice President, Sales, Sales Manager, and Inside Sales Coordinator */
